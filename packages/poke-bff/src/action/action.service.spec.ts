6d0f7774d4794e2643c5a1790ab8109348910a68
LucasSAmaral <lucassantosamaral@hotmail.com>
2023-02-12 18:51:21 -0300

    Cria testes para o método checkAnswer

--- packages/poke-bff/src/action/action.service.spec.ts
+++ packages/poke-bff/src/action/action.service.spec.ts
@@ -15,4 +15,57 @@ describe('ActionService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('checkAnswer method', () => {
+    it('should return game over equals true', () => {
+      const payload = {
+        correctAnswer: 'pikachu',
+        selectedAnswer: 'pinsir',
+        currentPoints: 0,
+      };
+
+      const checkedAnswer = service.checkAnswer(payload);
+
+      const expectedAnswer = { gameOver: true };
+
+      expect(checkedAnswer).toEqual(expectedAnswer);
+    });
+
+    it('should return correct answer', () => {
+      const payload = {
+        correctAnswer: 'pikachu',
+        selectedAnswer: 'pikachu',
+        currentPoints: 0,
+      };
+
+      const checkedAnswer = service.checkAnswer(payload);
+
+      const expectedAnswer = {
+        isAnswerCorrect: true,
+        correctAnswer: 'pikachu',
+        score: 1,
+        gameOver: false,
+      };
+
+      expect(checkedAnswer).toEqual(expectedAnswer);
+    });
+
+    it('should return wrong answer', () => {
+      const payload = {
+        correctAnswer: 'pikachu',
+        selectedAnswer: 'Elephant',
+        currentPoints: 10,
+      };
+
+      const checkedAnswer = service.checkAnswer(payload);
+
+      const expectedAnswer = {
+        wrongAnswer: 'Elephant',
+        score: 9,
+        gameOver: false,
+      };
+
+      expect(checkedAnswer).toEqual(expectedAnswer);
+    });
+  });
 });
