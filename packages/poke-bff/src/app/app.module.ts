21cbd1ae6ca49c9ab26eff1ce07590eeb7088339
LucasSAmaral <lucassantosamaral@hotmail.com>
2024-01-30 03:32:59 -0300

    Teste vercel

--- packages/poke-bff/src/app/app.module.ts
+++ packages/poke-bff/src/app/app.module.ts
@@ -4,12 +4,21 @@ import {
   RequestMethod,
   MiddlewareConsumer,
 } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { ActionModule } from '../action/action.module';
 import { BuildModule } from '../build/build.module';
 import { LoggerMiddleware } from '../middlewares/logger.middleware';
 
 @Module({
-  imports: [ActionModule, BuildModule],
+  imports: [
+    ActionModule,
+    BuildModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'poke-front', 'dist'),
+      exclude: ['poke-bff/*'],
+    }),
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
